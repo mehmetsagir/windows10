cfb7b73f91c6ebb789ec8a2275d0a95ee31ddd2a
Mehmet Sağır <62952843+mehmetsagir@users.noreply.github.com>
2021-05-16 23:17:02 +0300

    init

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
