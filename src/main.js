4834e6a29718935519e3314b91bceed89e1452c3
Mehmet Sağır <62952843+mehmetsagir@users.noreply.github.com>
2021-08-15 19:53:07 +0300

    Windows UI

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import wb from "./registerServiceWorker";
 import ClickOutside from "vue-click-outside";
+import "./registerServiceWorker";
 import("./helpers/rightClick");
 
 Vue.config.productionTip = false;
 Vue.directive("ClickOutside", ClickOutside);
+
+Vue.prototype.$workbox = wb;
 
 new Vue({
   store,
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  publicPath: "./",
+
+  pwa: {
+    themeColor: "#42b983",
+    msTileColor: "#42b983",
+    appleMobileWebAppCache: "yes",
+    manifestOptions: {
+      background_color: "#42b983",
+    },
+  },
+};
