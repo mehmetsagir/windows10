1ea9940ca06e385d4a7a3da5bf613a3c80152cd8
Mehmet <mehmet.sagir@ismakinesi.com>
2022-08-17 02:50:57 +0300

    Delete registerServiceWorker

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
-import wb from "./registerServiceWorker";
 import ClickOutside from "vue-click-outside";
 import VCalendar from "v-calendar";
-import("./registerServiceWorker");
 import("./helpers/rightClick");
 import("./assets/css/transition.css");
 
 Vue.config.productionTip = false;
 Vue.directive("ClickOutside", ClickOutside);
 
-Vue.prototype.$workbox = wb;
 Vue.component("VCalendar", VCalendar);
 
 Vue.use(VCalendar);
