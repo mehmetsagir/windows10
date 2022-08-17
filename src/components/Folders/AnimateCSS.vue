7e1feb45af587ce9bc11c39fc542562752218886
mehmetsagir <memetsagirint@gmail.com>
2022-08-17 03:07:44 +0300

    Change animate css app preview link

--- src/components/Folders/AnimateCSS.vue
+++ src/components/Folders/AnimateCSS.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout folderName="AnimateCSS">
-    <iframe src="https://animatecss-clone-ms.netlify.app/"></iframe>
+    <iframe src="https://animate-css-clone.vercel.app/"></iframe>
   </Layout>
 </template>
 
